6c8c51f1b9a66157edf085ce582cccadfec4cbf2
saleszera <crowzcoleslaw@gmail.com>
2020-03-25 11:24:01 -0300

    alterado o controller para uma classe e o require foi trocado por import/export

--- aulas/backend/src/controllers/SessionController.js
+++ aulas/backend/src/controllers/SessionController.js
@@ -1,15 +1,17 @@
-const connection = require('../database/connection');
+import connection from '../database/connection';
 
-module.exports = {
-  async create(req, res){
-    const {id} = req.body;
+class SessionController {
+  async create(req, res) {
+    const { id } = req.body;
 
     const ong = await connection('ongs').where('id', id).select('name').first();
 
-    if(!ong){
-      return res.status(400).json({Error: "no ONG found with this ID"})
+    if (!ong) {
+      return res.status(400).json({ Error: 'no ONG found with this ID' });
     }
 
     return res.json(ong);
   }
 }
+
+export default new SessionController();
