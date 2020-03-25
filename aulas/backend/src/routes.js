fae7eff6162007cf5b3f47ff395f3b49cd937993
saleszera <crowzcoleslaw@gmail.com>
2020-03-25 01:13:37 -0300

    adicionada rota que lista todos os casos criados pelo usuario logado

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentContoller');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
-routes.get('/incidents', IncidentController.index)
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes
