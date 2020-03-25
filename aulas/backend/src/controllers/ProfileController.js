2e6101d28c5d85a1a09ab31c9a755148d8593ac4
saleszera <crowzcoleslaw@gmail.com>
2020-03-25 01:12:26 -0300

    controler que lista todos os casos criados pelo usuario logado

---
+++ aulas/backend/src/controllers/ProfileController.js
@@ -0,0 +1,15 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(req, res){
+    const ong_id = req.headers.authorization;
+    console.log(ong_id);
+    const incidents = await connection('incidents')
+      .where('ong_id', ong_id)
+      .select('*');
+
+    console.log(incidents);
+
+    return res.json(incidents);
+  }
+}
