19beaa1805783c655f23799477fa8cf075c172f4
saleszera <crowzcoleslaw@gmail.com>
2020-03-25 00:48:31 -0300

    reformuladas as rotas para criação e listagem de ongs. Inseridas as rotas para criação, listagem e delete de casos

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -2,29 +2,16 @@ const express = require('express');
 const crypto = require('crypto');
 const connection = require('./database/connection');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentContoller');
+
 const routes = express.Router();
 
-routes.get('/ongs', async (req, res) => {
-  const ongs = await connection('ongs').select('*');
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-  return res.json(ongs)
-})
-
-routes.post('/ongs', async (req, res) => {
-  const {name, email, whatsapp, city, uf} = req.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  })
-
-  return res.json({id});
-});
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes
