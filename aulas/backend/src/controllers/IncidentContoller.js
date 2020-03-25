c14c3b3ee7db693330dba2c784e83b98cd622992
saleszera <crowzcoleslaw@gmail.com>
2020-03-25 01:41:25 -0300

    na listagem do caso agora é possivel ver os dados do usuario que criou o caso

--- aulas/backend/src/controllers/IncidentContoller.js
+++ aulas/backend/src/controllers/IncidentContoller.js
@@ -20,7 +20,11 @@ module.exports = {
 
     const [count] = await connection('incidents').count();
 
-    const incidents = await connection('incidents').limit(5).offset((page - 1) * 5).select('*');
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
 
     res.header('X-Total-Count', count['count(*)'])
 
