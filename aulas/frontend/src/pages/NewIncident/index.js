5ad6be1a65a1e88432a3e98767703d8b7eedf265
saleszera <crowzcoleslaw@gmail.com>
2020-03-26 12:08:04 -0300

    implementada a logica para a criação de um novo caso e quando o caso for cadastrado a pagina é redirecionada para a pagina raiz, caso haja algum erro, a pagina retorna um alert com o erro

--- aulas/frontend/src/pages/NewIncident/index.js
+++ aulas/frontend/src/pages/NewIncident/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 
@@ -15,6 +17,32 @@ export default function NewIncident() {
     alert('É necessário estar logado!');
 
     history.push('/');
+  }
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  async function handleAddIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Erro ao cadastrar caso, tente novamente!');
+    }
   }
 
   return (
@@ -34,10 +62,24 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input type="text" placeholder="Titulo do caso" />
-          <textarea placeholder="Descrição" />
-          <input type="text" placeholder="Valor em reais" />
+        <form onSubmit={handleAddIncident}>
+          <input
+            type="text"
+            placeholder="Titulo do caso"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Valor em reais"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <button className="button" type="submit">
             Cadastrar
