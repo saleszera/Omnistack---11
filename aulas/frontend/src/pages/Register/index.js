dd3b19d86257783fb6f6b95cd1b20ec018c9b4fb
saleszera <crowzcoleslaw@gmail.com>
2020-03-26 10:32:43 -0300

    implementado o redirecionamento para a pagina raiz após o cadastro de uma ONG

--- aulas/frontend/src/pages/Register/index.js
+++ aulas/frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -12,6 +12,8 @@ export default function Register() {
   const [whatsapp, setWhatsapp] = useState('');
   const [city, setCity] = useState('');
   const [uf, setUf] = useState('');
+
+  const history = useHistory();
 
   async function handleRegister(e) {
     e.preventDefault();
@@ -28,6 +30,8 @@ export default function Register() {
       const response = await api.post('ongs', data);
 
       alert(`Seu ID de acesso: ${response.data.id}`);
+
+      history.push('/');
     } catch (err) {
       alert('Erro no cadastro, tente novamente');
     }
