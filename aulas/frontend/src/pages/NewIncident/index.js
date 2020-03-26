57ca1bbfebfe6a13482aa74b32a822e06986fee8
saleszera <crowzcoleslaw@gmail.com>
2020-03-26 11:45:06 -0300

    implementada verificação se a ONG está logada no sistema, caso não esteja, a ONG será redirecionada para a pagina raiz

--- aulas/frontend/src/pages/NewIncident/index.js
+++ aulas/frontend/src/pages/NewIncident/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -7,6 +7,16 @@ import logoImg from '../../assets/images/logo.svg';
 import './styles.css';
 
 export default function NewIncident() {
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  if (ongId === null) {
+    alert('É necessário estar logado!');
+
+    history.push('/');
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
